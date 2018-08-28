201cff171c9ec732cf5fad71f044f53481389cb0
Carlos <blancobc@unican.es>
2018-08-28 23:49:49 +0200

    intents

---
+++ app/src/main/java/com/isunican/gastoporviajero/AcercaDe.java
@@ -0,0 +1,17 @@
+package com.isunican.gastoporviajero;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+
+public class AcercaDe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acerca_de);
+
+    }
+
+}
--- app/src/main/java/com/isunican/gastoporviajero/MainActivity.java
+++ app/src/main/java/com/isunican/gastoporviajero/MainActivity.java
@@ -1,14 +1,18 @@
 package com.isunican.gastoporviajero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // Declaramos variables para los elementos (vistas) de la interfaz
     TextView txtKm, txtPrecio, txtViajeros, txtConsumo, txtResultado;
     Button btnCalcular;
 
@@ -18,20 +22,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Al iniciar la actividad vinculamos los elementos de la interfaz con las variables declaradas anteriormente
+        // Para ello buscamos los elementos por id
         txtKm = findViewById(R.id.txtKm);
         txtPrecio = findViewById(R.id.txtPrecio);
         txtViajeros = findViewById(R.id.txtViajeros);
         txtConsumo = findViewById(R.id.txtConsumo);
         txtResultado = findViewById(R.id.txtResultado);
 
+        // Para el boton además definimos un listener
         btnCalcular = findViewById(R.id.btnCalcular);
         btnCalcular.setOnClickListener(this);
     }
 
+
+    // Cuando se lanza un evento de click comprobamos sobre quién ha sido (qué vista)
     public void onClick(View v){
+
+        // Para ellos miramos su id
+        // Si es el botón calcular procedemos a realizar el cálculo
         if(v.getId()==R.id.btnCalcular){
 
-            //GastoViajero=((consumo/100)*kmRecorridos*precioLitro)/numeroViajeros
+            //Realizamos comprobaciones para ver si en todos los campos de texto
+            //han introducido algún valor numérico positivo
+            //En caso de no ser así, mostramos un mensaje de error emergente
 
             if( Float.valueOf(txtConsumo.getText().toString()) <= 0 ) {
                 Toast.makeText(this, R.string.error_consumo, Toast.LENGTH_SHORT).show();
@@ -45,6 +59,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             else if( Float.valueOf(txtViajeros.getText().toString()) <= 0 ) {
                 Toast.makeText(this, R.string.error_viajeros, Toast.LENGTH_SHORT).show();
             }
+
+            //Si todos los valores son positivos realizamos el cálculo,
+            //escribimos el resultado en el campo de texto que creamos para ello
+            //y mostramos un mensaje emergente
+
             else {
                 float gasto = (
                         ( Float.valueOf(txtConsumo.getText().toString()) / 100 )
@@ -56,14 +75,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 txtResultado.setText(String.valueOf(gasto));
 
                 Toast.makeText(this, android.R.string.ok, Toast.LENGTH_SHORT).show();
+                Log.d("operacion","operacion correcta");
             }
         }
 
 
         else{
             Toast.makeText(this, android.R.string.no, Toast.LENGTH_SHORT).show();
+            Log.d("operacion","operacion incorrecta");
         }
 
 
     }
+
+    // Metodo para lanzar la actividad acercaDe
+    // importar el paquete android.content.Intent
+    public void lanzarAcercaDe(View view) {
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
+    }
+
+
 }
