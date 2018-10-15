2be199c5703b618fe44c7393e79cc1bb0b239e8e
Pablo Sanchez <p.sanchez@unican.es>
2018-10-15 16:38:30 +0200

    Prueba 1.3

--- app/src/test/java/com/isunican/gastoporviajero/ExampleUnitTest.java
+++ app/src/test/java/com/isunican/gastoporviajero/ExampleUnitTest.java
@@ -14,4 +14,11 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void fail() {
+        assertEquals(0,1);
+    }
+
+
 }
